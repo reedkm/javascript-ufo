e89cdc9fe19140e8af926130b1d31f82594a758f
Kenneth Reed <kenneth@Kenneths-iMac.fios-router.home>
2018-11-17 22:13:36 -0500

    Final; appended data browser only

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,19 @@
-// Kenneth Reed
+// Variable for data
+var tableData = data
 
-var tableData = data;
-
+// Variable for tbody
 var tbody = d3.select("#body");
-
+// Select the input elements
 var reset = d3.select("#reset-btn");
+
+// Select the input elements
+var dateInput = d3.select("#datetime");
+var cityInput = d3.select("#city");
+var stateInput = d3.select("#state");
+var countryInput = d3.select("#country");
+var shapeInput = d3.select("#shape");
+var durationInput = d3.select("#duration");
+var commentaryInput = d3.select("#commentary");
 
 reset.on("click", function() {
 	buildTable();
@@ -40,33 +49,30 @@ function clearTable() {
 	})
 }
 
-// Filter the table for date #################################
-var filterDate = d3.select("#filter-date");
+// Filter the table #################################
+var filter = d3.select("#filter-btn");
 
-filterDate.on("click", function() {
-	filterTableDate();
+filter.on("click", function() {
+	filterTable();
 });
 
-// filterTableDate filters by date input
-function filterTableDate() {
+// Filter table by input
+function filterTable() {
 	// Prevent the page from refreshing
 	d3.event.preventDefault();
 	
 	//clears previous filter
-	tbody.text("<!-- Cleared? -->")
-	 
-	// Prevent the page from refreshing
-	d3.event.preventDefault();
-
-	// Select the input element and get the raw HTML node
-	var inputElement = d3.select("#datetime");
+	tbody.text("")
 	
-	// Get the value property of the input element
-	var inputValue = inputElement.property("value");
-	var cityInputValue = inputElement.property("value");
+	// Prevent the page from refreshing
+	d3.event.preventDefault();	
+	
+	// Filter for date
+	if (dateInput != "") {
 
-	var filteredData = tableData.filter(data => data.datetime === inputValue);
+	var dateInputValue = dateInput.property("value");
 
+	var filteredData = tableData.filter(data => data.datetime === dateInputValue);
 	// Loop to build filtered table body
 	filteredData.forEach(function(xFileDate) {
 		var row = tbody.append('tr');	
@@ -74,5 +80,109 @@ function filterTableDate() {
 			var cell = row.append('td');
 			cell.text(value);
 	 });
-	})
+	})	
+	}
+	// Filter for city
+	if (cityInput != "") {
+
+	var cityInputValue = cityInput.property("value");
+
+	var filteredData = tableData.filter(data => data.city === cityInputValue);
+		// Loop to build filtered table body
+	filteredData.forEach(function(xFileDate) {
+		var row = tbody.append('tr');	
+		Object.entries(xFileDate).forEach(function([key, value]) {
+			var cell = row.append('td');
+			cell.text(value);
+	 });
+	})	
+	}
+	// Filter for state
+	if (stateInput != "") {
+
+	var stateInputValue = stateInput.property("value");
+
+	var filteredData = tableData.filter(data => data.state === stateInputValue);
+		// Loop to build filtered table body
+	filteredData.forEach(function(xFileDate) {
+		var row = tbody.append('tr');	
+		Object.entries(xFileDate).forEach(function([key, value]) {
+			var cell = row.append('td');
+			cell.text(value);
+	 });
+	})	
+	}
+	 // Filter for country
+	if (countryInput != "") {
+
+	var countryInputValue = countryInput.property("value");
+
+	var filteredData = tableData.filter(data => data.state === countryInputValue);
+		// Loop to build filtered table body
+	filteredData.forEach(function(xFileDate) {
+		var row = tbody.append('tr');	
+		Object.entries(xFileDate).forEach(function([key, value]) {
+			var cell = row.append('td');
+			cell.text(value);
+	 });
+	})	
+	}
+	// Filter for shape
+	if (shapeInput != "") {
+
+	var shapeInputValue = shapeInput.property("value");
+
+	var filteredData = tableData.filter(data => data.shape === shapeInputValue);
+		// Loop to build filtered table body
+	filteredData.forEach(function(xFileDate) {
+		var row = tbody.append('tr');	
+		Object.entries(xFileDate).forEach(function([key, value]) {
+			var cell = row.append('td');
+			cell.text(value);
+	});
+	})	
+	}
 }
+
+// Append new sightings #################################
+var append = d3.select("#append-btn");
+
+append.on("click", function() {
+	appendTable();
+});
+
+// Append table by input
+function appendTable() {
+   
+	// Prevent the page from refreshing
+	d3.event.preventDefault();
+	
+	// Set form inputs for values
+	var dateInputValue = dateInput.property("value");
+	var cityInputValue = cityInput.property("value");
+	var stateInputValue = stateInput.property("value");
+	var countryInputValue = countryInput.property("value");
+	var shapeInputValue = shapeInput.property("value");
+	var durationInputValue = durationInput.property("value");
+	var commentaryInputValue = commentaryInput.property("value");
+
+	// Append values
+	var inputs =[];
+	inputs.push({"datetime": dateInputValue});
+	inputs.push({"city": cityInputValue});
+	inputs.push({"state": stateInputValue});
+	inputs.push({"country": countryInputValue});
+	inputs.push({"shape": shapeInputValue});
+	inputs.push({"duration": durationInputValue});
+	inputs.push({"comments": commentaryInputValue});
+	// Push values
+	data.push(inputs);
+
+		var row = tbody.append('tr');
+		Object.entries(inputs).forEach(function([key, value]) {
+			var cell = row.append('td');
+			cell.text(Object.values(value)[0]);
+		});
+}
+
+// Can write to table in browser, but JavaScript cannot write to hardrive, so data is lost on reset
